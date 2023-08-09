78ffeb218d411218a1afba8298045766c3e8ba04
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-08-09 09:44:02 +0200

    Fix Hamiltonian variables.

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -3,7 +3,9 @@
 
 function hamiltonian_variables(dimension::Int)
     t = parameter(:t)
-    q = @variables (q(t))[1:dimension]
-    p = @variables (p(t))[1:dimension]
+    
+    @variables (q(t))[1:dimension]
+    @variables (p(t))[1:dimension]
+    
     return (t,q,p)
 end
