fd0bde9e1232dd9c63f45e2a2622d8889c045890
Documenter.jl <documenter@juliadocs.github.io>
2023-09-20 17:22:47 +0000

    build based on 54a0c6a

---
+++ v0.3.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EulerLagrange","category":"page"},{"location":"#EulerLagrange","page":"Home","title":"EulerLagrange","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EulerLagrange]","category":"page"}]
+}
