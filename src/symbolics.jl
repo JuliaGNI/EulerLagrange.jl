5451a9aeb6f9b4f6817c3750f78767a61cd5bf4b
Michael Kraus <michael.kraus@ipp.mpg.de>
2024-01-18 14:03:03 +0100

    Fix bug for systems with exactly one parameter.

--- src/symbolics.jl
+++ src/symbolics.jl
@@ -4,13 +4,21 @@ parameter(name::Symbol) = Num(Sym{Real}(name))
 
 function substitute_parameters(code, params)
     if length(params) > 0
+        # generate string of parameter arguments as they appear in generated code
         paramstr = string(Tuple("params_" * string(k) * ", " for k in keys(params))...)
+        # remove trailing comma
         paramstr = paramstr[begin:prevind(paramstr, first((findlast(",", paramstr))))]
-        
+        # convert code expression to string
         code_str = string(code)
-        code_str = replace(code_str, paramstr => "params")
+        # extract function header
+        func_str = code_str[first(findfirst("function (", code_str)):last(findfirst(paramstr * ")", code_str))]
+        # replace parameter list in function header with `params`
+        func_str_params = replace(func_str, paramstr * ")" => "params)")
+        # replace function header in code
+        code_str = replace(code_str, func_str => func_str_params)
+        # replace all remaining params with named tuple entries
         code_str = replace(code_str, "params_" => "params.")
-
+        # convert code string back to expression
         return Meta.parse(code_str)
     else
         return code
